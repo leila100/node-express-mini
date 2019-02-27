a9e1930e35ad38552735597d07380e93e97a9f3b
Leila <nb.leila10@gmail.com>
2019-02-27 11:11:11 -0600

    Implement DELETE API
    Removes the user with the specified id
    Endpoint: /api/users/:id
    Returns the deleted user.

--- index.js
+++ index.js
@@ -104,6 +104,46 @@ server.put("/api/users/:id", (req, res) => {
   }
 })
 
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params
+  if (isNaN(id)) {
+    res.status(400).json({ errorMessage: "The id has to be a number" }) // 400: Bad Request
+  } else {
+    //Retrieve the user info before deleting it
+    let deletedUser = {}
+    db.findById(id)
+      .then(user => {
+        if (user) deletedUser = user
+        else {
+          res
+            .status(404) // 404: Not Found
+            .json({ message: "The user with the specified ID does not exist." })
+        }
+      })
+      .catch(err =>
+        res
+          .status(500) // 500: Server Error
+          .json({ error: "The user information could not be retrieved." })
+      )
+
+    db.remove(id)
+      .then(response => {
+        if (response) {
+          res.status(200).json(deletedUser) // 200: OK
+        } else {
+          res
+            .status(404) // 404: Not Found
+            .json({ message: "The user with the specified ID does not exist." })
+        }
+      })
+      .catch(err =>
+        res
+          .status(500) // 500: Server Error
+          .json({ error: "The user could not be removed" })
+      )
+  }
+})
+
 server.listen(PORT, () => {
   console.log("Server is listening on port ", PORT)
 })
