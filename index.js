303acacf5adcec316ca58b4483a169c4d4b36ae9
Leila <nb.leila10@gmail.com>
2019-02-27 10:53:45 -0600

    Implement PUT API
    Updates the user with the specified id using data from the request body.
    Endpoint: /api/users/:id
    Returns the modified document, NOT the original.

--- index.js
+++ index.js
@@ -67,6 +67,43 @@ server.post("/api/users", (req, res) => {
   }
 })
 
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params
+  const userInfo = req.body
+  if (isNaN(id)) {
+    res.status(400).json({ errorMessage: "The id has to be a number" }) // 400: Bad Request
+  } else if (userInfo.name && userInfo.bio) {
+    db.update(id, userInfo)
+      .then(response => {
+        db.findById(id)
+          .then(user => {
+            // 200: OK
+            if (user) res.status(200).json(user)
+            else
+              res
+                .status(404) // 404: Not Found
+                .json({
+                  message: "The user with the specified ID does not exist."
+                })
+          })
+          .catch(err =>
+            res
+              .status(500) // 500: Server Error
+              .json({ error: "The user information could not be retrieved." })
+          )
+      })
+      .catch(err =>
+        res
+          .status(500) // 500: Server Error
+          .json({ error: "The user information could not be modified." })
+      )
+  } else {
+    res
+      .status(400) // 400: Bad Request
+      .json({ errorMessage: "Please provide name and bio for the user." })
+  }
+})
+
 server.listen(PORT, () => {
   console.log("Server is listening on port ", PORT)
 })
