4e7aef470e80d53379d52584d443c0485640c0e1
Leila <nb.leila10@gmail.com>
2019-02-27 09:59:32 -0600

    Implement GET API
    Returns the user object with the specified id
    Endpoint: /api/users/:id

--- index.js
+++ index.js
@@ -4,10 +4,37 @@ const db = require("./data/db")
 const server = express()
 const PORT = 8080
 
+server.use(express.json())
+
 server.get("/api/users", (req, res) => {
   db.find()
     .then(users => res.json(users))
-    .catch(err => res.status(400).json({ err: err }))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." })
+    )
+})
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params
+  if (isNaN(id)) {
+    res.status(500).json({ error: "The id has to be a number" })
+  } else {
+    db.findById(id)
+      .then(user => {
+        if (user) res.json(user)
+        else
+          res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist." })
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "The user information could not be retrieved." })
+      )
+  }
 })
 
 server.listen(PORT, () => {
