d5095a431b645e9a20dae98de8c2c668245e7536
Leila <nb.leila10@gmail.com>
2019-02-27 10:32:11 -0600

    Implement POST API
    Creates a user using the information sent inside the request body
    Endpoint: /api/users
    Returns the newly created user

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.get("/api/users", (req, res) => {
     .then(users => res.json(users))
     .catch(err =>
       res
-        .status(500)
+        .status(500) // 500: Server Error
         .json({ error: "The users information could not be retrieved." })
     )
 })
@@ -19,21 +19,51 @@ server.get("/api/users", (req, res) => {
 server.get("/api/users/:id", (req, res) => {
   const { id } = req.params
   if (isNaN(id)) {
-    res.status(500).json({ error: "The id has to be a number" })
+    res.status(400).json({ errorMessage: "The id has to be a number" }) // 400: Bad Request
   } else {
     db.findById(id)
       .then(user => {
         if (user) res.json(user)
         else
           res
-            .status(404)
+            .status(404) // 404: Not Found
             .json({ message: "The user with the specified ID does not exist." })
       })
       .catch(err =>
         res
-          .status(500)
+          .status(500) // 500: Server Error
           .json({ error: "The user information could not be retrieved." })
       )
+  }
+})
+
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body
+
+  if (userInfo.name && userInfo.bio) {
+    db.insert(userInfo)
+      .then(({ id }) => {
+        db.findById(id)
+          .then(user => {
+            res.status(201).json(user)
+          }) // 201: Created
+          .catch(err =>
+            res
+              .status(500) // 500: Server Error
+              .json({ error: "The user information could not be retrieved." })
+          )
+      })
+      .catch(err =>
+        res
+          .status(500) // 500: Server Error
+          .json({
+            error: "There was an error while saving the user to the database"
+          })
+      )
+  } else {
+    res
+      .status(400) // 400: Bad Request
+      .json({ errorMessage: "Please provide name and bio for the user." })
   }
 })
 
