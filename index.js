25f0991f8bf3eb849977488b630f9d2563c16aee
Leila <nb.leila10@gmail.com>
2019-02-27 09:22:29 -0600

    Setup server listening on port 8080

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// implement your API here
+const express = require("express")
+
+const server = express()
+const PORT = 8080
+
+server.listen(PORT, () => {
+  console.log("Server is listening on port ", PORT)
+})
