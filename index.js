9dca4a477e6d4d72aa501865cb27ed3778b69334
Leila <nb.leila10@gmail.com>
2019-02-27 09:37:10 -0600

    Implement the GET API
    Returns an array of all the user objects contained in the database.
    Enpoint: /api/users

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express")
+const db = require("./data/db")
 
 const server = express()
 const PORT = 8080
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json({ err: err }))
+})
 
 server.listen(PORT, () => {
   console.log("Server is listening on port ", PORT)
