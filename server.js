60c6bec02797022cfce5ef6b07937b7602af1ccc
Leila <nb.leila10@gmail.com>
2019-03-07 10:46:30 -0600

    Prepare for deployment
    Add start script
    Add morgan and helmet
    Change PORT to get from precess.env

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express")
 const cors = require("cors")
+const logger = require("morgan")
+const helmet = require("helmet")
+
 const userRouter = require("./user/user-router")
 
 const server = express()
 
+server.use(helmet())
 server.use(express.json())
+server.use(logger("dev"))
 server.use(cors())
 
 server.use("/api/users", userRouter)
+
+server.get("/", (req, res) => {
+  res.status(200).json("Welcome to Users App")
+})
 
 module.exports = server
